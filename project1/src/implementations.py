1877f9b348e5683608e629ee58e2b6123de269bf
mantheflan <auguste.apple@gmail.com>
2020-10-01 13:42:12 +0200

    Added Least squares gradient descent algorithm

--- project1/src/implementations.py
+++ project1/src/implementations.py
@@ -1,6 +1,17 @@
+import numpy as np
+from helpers import *
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
   """Linear regression using gradient descent"""
+  N = len(y)
+  w = initial_w
+
+  for n_iter in range(max_iters):
+    gradient = -1/N * tx.T @ (y - tx @ w)
+    w = w - gamma * gradient
+    # print("Gradient Descent({bi}/{ti}): loss={l}".format( bi=n_iter, ti=max_iters - 1, l=(1/(2*N) * (y - tx.T @ w).T @ (y - tx.T @ w))))
+
+  loss = compute_mse_loss(y, tx, w)
   return (w, loss)
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
