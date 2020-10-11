10338d1d9386ff906b5998d121c8e651b6457581
Alexander Apostolov <alexander.apostolov@epfl.ch>
2020-10-11 15:45:25 +0200

    minus sign removed from MSE

--- project1/src/proj1_helpers.py
+++ project1/src/proj1_helpers.py
@@ -40,7 +40,7 @@ def compute_mse_loss(y, tx, w):
   """  
   e = y - tx @ w
   print((e.T @ e).shape)
-  return (-1/(2*len(y)) * e.T @ e).item()
+  return (1/(2*len(y)) * e.T @ e).item()
 
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
