fab3c1cdff0d7f525cbb3050e1cae1e0cf70f1a4
mantheflan <auguste.apple@gmail.com>
2020-10-12 10:58:54 +0200

    Added cross_validation_SGD

--- project1/src/proj1_helpers.py
+++ project1/src/proj1_helpers.py
@@ -61,6 +61,93 @@ def compute_ridge_loss(y, tx, w, lambda_):
   e = y - tx @ w
   return (1./(2*len(y)) * e.T @ e + lambda_ * w.T @ w).item() 
 
+
+def build_k_indices(y, K, seed):
+  """Build k indices for k-fold cross-validation.
+
+  Parameters
+  ----------
+  y : numpy array
+    Targets vector (N,1)
+  K : int
+    Number of folds
+  seed : int
+    Seed for index shuffling
+  Returns
+  -------
+  res : numpy array
+    2-dimensional array with shuffled indices arranged in K rows
+  """
+  num_row = y.shape[0]
+  interval = int(num_row / K)
+  np.random.seed(seed)
+  # Shuffle (1, ..., num_row)
+  indices = np.random.permutation(num_row)
+  # Arrange indices into K lists
+  k_indices = [indices[
+    k * interval: (k + 1) * interval
+  ] for k in range(K)]
+  res = np.array(k_indices)
+  return res
+
+
+def cross_validation_SGD(y, tx, K, B, gamma, seed):
+  """K-fold cross validation for stochastic gradient descent.
+
+  Parameters
+  ----------
+  y : numpy array
+    Targets vector (N,1)
+  tx : numpy array
+    Feature matrix (N,D)
+  K : int
+    Number of folds
+  B : int
+    Size of mini-batches
+  gamma : float
+    Learning rate for SGD
+  seed : int
+    Seed for index shuffling
+  Returns
+  -------
+  w_best : numpy array
+    Weight vector (D,1) with smallest validation error
+  training_errors : list
+    Training error for each fold (K elements)
+  validation_errors : list
+    Validation error for each fold (K elements)
+  """
+  k_indices = build_k_indices(y, K, seed)
+
+  training_errors = []
+  validation_errors = []
+  min_error = 1e16 # As large as possible
+
+  for k in range(K):
+    # Take the k-th row of tx and y
+    f = lambda a: a[k_indices[k]][:]
+    tx_train, y_train = map(f, (tx, y))
+
+    # Take all but the k-th row of tx and y
+    f = lambda a: a[ np.concatenate( [ k_indices[i] for i in range(len(k_indices)) if i != k ] )][:]
+    tx_test, y_test = map(f, (tx, y))
+
+    # Train
+    # w, loss_tr = SGD(y_train, tx_train, gamma, B)
+    # Test
+    # How do I get the loss? It's in SGD
+    # loss_te = algo_loss(y_test, tx_test, w)
+
+    training_errors.append(loss_tr)
+    validation_errors.append(loss_te)
+
+    if loss_te < min_error:
+      min_error = loss_te
+      w_best = w
+
+  return w_best, training_errors, validation_errors
+
+
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
     y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
